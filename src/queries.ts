68fc5af4f5e12021ab58c878316222070026e834
Stephen <teamgreensf@gmail.com>
2022-03-25 20:27:22 -0700

    Dev and Prod modes for easy deployment

--- src/queries.ts
+++ src/queries.ts
@@ -1,13 +1,24 @@
-import { Pool } from "pg";
+import { Pool, PoolConfig } from "pg";
 import { Request, Response } from 'express';
 
-const pool = new Pool({
+var dbInfo: PoolConfig = {
     user: 'proreport',
     host: 'localhost',
     database: 'proreport',
     password: 'test',
     port: 5432
-});
+};
+
+if (process.env.NODE_ENV === 'production') {
+    dbInfo = {
+        user: 'proreport',
+        host: '/var/run/postgresql',
+        database: 'proreport',
+        port: 5432
+    }
+}
+
+const pool = new Pool(dbInfo);
 
 export const getUsers = (request: Request, response: Response) => {
     pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
