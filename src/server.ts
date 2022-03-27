814dab667d808b522849dad6ccaf30c2d648f14c
Stephen <teamgreensf@gmail.com>
2022-03-27 03:52:27 -0700

    Basic backend api functionality

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { response } from 'express';
 import { Request, Response } from 'express';
+import { PS_RoutingRow, PS_TrackingRow, PS_WorkOrder, sequelize } from './db';
 import * as db from './queries';
 
 const app = express();
@@ -12,9 +13,61 @@ app.get('/', (request: Request, response: Response) => {
     response.json({ info: 'Node.js server functioning normally' });
 });
 
-app.get('/users', db.getUsers);
+app.get('/workorders', getAllWorkOrders);
+app.get('/workorders/:index', getWorkOrderByIndex);
+
+app.get('/routingrows', getAllRoutingRows);
+app.get('/routingrows/:workOrderId', getRoutingRowsByWorkOrderId);
+
+app.get('/trackingrows', getAllTrackingRows);
+app.get('/trackingrows/:workOrderId', getTrackingRowsByWorkOrderId);
 
 
 app.listen(port, () => {
+    sequelize.sync();
     console.log(`Server listening on port ${port}`);
 });
+
+function getAllWorkOrders(request: Request, response: Response) {
+    PS_WorkOrder.findAll().then(results => {
+        response.status(200).json(results);
+        console.log(...results.map(e => e.id));
+    });
+}
+
+function getWorkOrderByIndex(request: Request, response: Response) {
+    PS_WorkOrder.findAll({ where: { index: request.params.index } }).then(results => {
+        response.status(200).json(results);
+    })
+
+}
+
+function getAllRoutingRows(request: Request, response: Response) {
+    PS_RoutingRow.findAll().then(results => {
+        response.status(200).json(results);
+    });
+}
+
+function getRoutingRowsByWorkOrderId(request: Request, response: Response) {
+    PS_RoutingRow.findAll({
+        where: { 
+            workOrderId: request.params.workOrderId 
+        },
+        attributes: ['op', 'opDesc', 'resource', 'completeTotal', 'completeDate'],
+        order: [['id', 'ASC']]
+    }).then(results => {
+        response.status(200).json(results);
+    });
+}
+
+function getAllTrackingRows(request: Request, response: Response) {
+    PS_TrackingRow.findAll().then(results => {
+        response.status(200).json(results);
+    });
+}
+
+function getTrackingRowsByWorkOrderId(request: Request, response: Response) {
+    PS_TrackingRow.findAll({ where: { workOrderId: request.params.workOrderId } }).then(results => {
+        response.status(200).json(results);
+    });
+}
